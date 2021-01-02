ac8844c1c4b38cbb238755b3115f22c3f4b0402f
Jobert Sá <jobertsa@gmail.com>
2021-01-02 20:15:45 +0100

    #1 Initial project structure

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FoundationExtensions",
+    products: [
+        .library(
+            name: "FoundationExtensions",
+            targets: ["FoundationExtensions"]),
+    ],
+    targets: [
+        .target(
+            name: "FoundationExtensions",
+            dependencies: []),
+        .testTarget(
+            name: "FoundationExtensionsTests",
+            dependencies: ["FoundationExtensions"]),
+    ]
+)
